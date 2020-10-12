034936979f73d015e0df744afeee195de181038c
Joeway <joewaybanh@hotmail.com>
2020-10-12 16:02:08 +0200

    added the the navbar and styling to it

---
+++ simpsons-quotes/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar/>
+        <p>
+          asd
+        </p>
+        <a 
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ simpsons-quotes/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className="Navbar">
+            <nav>
+                <a href="https://en.wikipedia.org/wiki/The_Simpsons">Simpsons Quotes</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
