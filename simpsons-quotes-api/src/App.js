7618fe13e0fd991b86d93391f405d51ac8c21d13
Joeway <joewaybanh@hotmail.com>
2020-10-31 21:01:00 +0100

    added initial state to be null and ternary in the render

--- simpsons-quotes-api/src/App.js
+++ simpsons-quotes-api/src/App.js
@@ -3,21 +3,27 @@ import './App.css';
 import axios from 'axios'
 import QuoteCard from './components/QuoteCard';
 
-const sampleQuote = {
-  quote: "By chilling my loins I increase the chances of impregnating my wife.",
-  character: "Apu Nahasapeemapetilon",
-  image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
-  characterDirection: "Left"
-}
+// const sampleQuote = {
+//   quote: "By chilling my loins I increase the chances of impregnating my wife.",
+//   character: "Apu Nahasapeemapetilon",
+//   image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
+//   characterDirection: "Left"
+// }
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote: sampleQuote
+      quote: null
     }
     this.getQuote = this.getQuote.bind(this);
   }
+
+
+  componentDidMount(){
+    this.getQuote()
+  }
+
 
   getQuote() {
     axios.get("https://simpsons-quotes-api.herokuapp.com/quotes")
@@ -31,7 +37,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <QuoteCard quote = {this.state.quote}/>
+        {this.state.quote ? (
+        <QuoteCard quote = {this.state.quote}/>) :
+        <p>Looking for quote..</p> }
         <button type="button" onClick={this.getQuote}>Get new quote!</button>
       </div>
     );
